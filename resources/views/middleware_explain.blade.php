e3e0c8d41f6688e79c5e806f2b990486babe7e86
agarciago20dw <agarciago20dw@ikzubirimanteo.com>
2022-02-01 11:36:51 +0100

    Finalizando explicación del middleware

--- resources/views/middleware_explain.blade.php
+++ resources/views/middleware_explain.blade.php
@@ -6,6 +6,6 @@
 
   <h2>Middleware explain</h2>
 
-  <p>En este middleware la función 'handle' verifica si el usuario está suscrito (boolean), si lo está la ejecución del programa seguirá su curso (las rutas que implementen este middleware seguirán con su ejecución), si no lo está se retornará un error 403.</p>
+  <p>En este middleware la función 'handle' verifica si el usuario está suscrito (atributo boolean del usuario), si lo está la ejecución del programa seguirá su curso (con 'return $next($request)'), si no lo está se retornará un error 403.</p>
 
 @endsection
